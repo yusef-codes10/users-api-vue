9a82250494448bfc47f6349d2f52ac7708d89608
Youcef <youcefcopy@gmail.com>
2026-02-08 18:17:58 +0100

    defining the onMounted async promise

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,16 +1,19 @@
 <script setup>
-import { ref } from 'vue'
-
-const firstName = ref('Jessica')
-const lastName = ref('Anderson')
-const age = ref(19)
-const gender = ref('Female')
+import { ref, onMounted } from 'vue'
 
 // create users array
 const users = ref([
   { firstName: 'Jessica', lastName: 'Anderson', age: 19, gender: 'Female' },
   { firstName: 'Alice', lastName: 'Stones', age: 18, gender: 'Female' },
 ])
+
+// fetching data from an api
+onMounted(() => { async () => {
+  const response = await fetch('https://dummyjson.com/users?limit=10')
+  const data = await response.json()
+}
+
+}),
 </script>
 
 <template>
