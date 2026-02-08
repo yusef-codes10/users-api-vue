c160b6046f1027a27eb9ea974f47d8fc5701c725
Youcef <youcefcopy@gmail.com>
2026-02-08 18:02:08 +0100

    user layout

---
+++ src/components/User.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <h1>User 1</h1>
+  <ul>
+    <li>First name:</li>
+    <li>Last name:</li>
+    <li>age:</li>
+    <li>gender:</li>
+  </ul>
+</template>
+
+<style scoped></style>
